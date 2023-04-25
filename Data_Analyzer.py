5a3dfa5df555f40d9f598a22efad9333ba193edb
Arimawn <dv.armangh@gmail.com>
2023-04-25 13:43:26 +0330

    Update

--- Data_Analyzer.py
+++
@@ -1,5 +0,0 @@
-with open("B:/Programming Projects/Pyhton/Twitter Cracker/New Version/final_data.txt","r") as file:
-    for line in file:
-        username,password,email,emailpass = line.strip().split(":",maxsplit=4)
-        print(f"{username}:{password}:{email},{emailpass}")
-
